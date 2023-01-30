3d7ba86e5e1b84f1d3d9803a9ffd415abf2ba561
RazaNaqsh <owaisraza1704@gmail.com>
2023-01-30 14:19:51 +0530

    mainPage main section added

--- src/initialpage.js
+++ src/initialpage.js
@@ -1,4 +1,5 @@
-import mythril from "./imgs/Mythril.png";
+import mythrilImg from "./imgs/Mythril.png";
+import challeImg from "./imgs/Challe.png";
 
 export default function intialLoad() {
 	const content = document.getElementById("content");
@@ -7,15 +8,46 @@ export default function intialLoad() {
 	const header = document.createElement("header");
 	header.classList.add("content__header");
 	// header-title
-	const title = document.createElement("h1");
-	title.textContent = "Challe Fen Challe";
-	title.classList.add("header__title");
-	header.append(title);
+	const headerTitle = document.createElement("h1");
+	headerTitle.textContent = "Challe Fen Challe";
+	headerTitle.classList.add("header__title");
+	header.append(headerTitle);
 	content.append(header);
 
 	// header-img
-	const mythrilImg = new Image();
-	mythrilImg.src = mythril;
-	header.append(mythrilImg);
-	mythrilImg.classList.add("header__mythril");
+	const mythril = new Image();
+	mythril.src = mythrilImg;
+	header.append(mythril);
+	mythril.classList.add("header__mythril");
+
+	// header-nav
+	const navBar = document.createElement("nav");
+
+	// main
+	const main = document.createElement("main");
+	main.classList.add("main");
+	content.append(main);
+
+	// main-challe
+	const challe = new Image();
+	challe.src = challeImg;
+	challe.classList.add("main__challe");
+	main.append(challe);
+
+	// main-textSection
+	const mainTextSection = document.createElement("section");
+	mainTextSection.classList.add("main__textSection");
+	main.append(mainTextSection);
+
+	// main-title
+	const maintitle = document.createElement("h2");
+	mainTextSection.append(maintitle);
+	maintitle.classList.add("main__title");
+	maintitle.textContent = "Welcome to the Warrior Fairy Restaurant~";
+
+	// main-textContent
+	const mainPara = document.createElement("p");
+	mainPara.classList.add("main__text");
+	mainTextSection.append(mainPara);
+	mainPara.textContent = "Would you like some sugar silver?";
 }
