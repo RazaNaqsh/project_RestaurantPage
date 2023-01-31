a91320e7ff95090b67fff1e22be308917e9543f1
RazaNaqsh <owaisraza1704@gmail.com>
2023-01-31 08:38:17 +0530

    Linked and styled  images in every page

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,28 @@
 import headerLoader from "./header";
+import cattImage from "./imgs/catt.png";
 
 export default function contactPageLoad() {
 	headerLoader();
 	const content = document.getElementById("content");
-	const menuMain = document.createElement("main");
-	menuMain.classList.add("menu__main");
-	content.append(menuMain);
+
+	// contact-main
+	const contactMain = document.createElement("main");
+	contactMain.classList.add("contact__main");
+	content.append(contactMain);
+
+	// contact-textSectionLeft
+	const contactTextSectionLeft = document.createElement("section");
+	contactTextSectionLeft.classList.add("contact__textSection--left");
+	contactMain.append(contactTextSectionLeft);
+
+	// catt-img
+	const catt = new Image();
+	catt.src = cattImage;
+	catt.classList.add("contact__catt");
+	contactMain.append(catt);
+
+	// contact-textSectionRight
+	const contactTextSectionRight = document.createElement("section");
+	contactTextSectionRight.classList.add("contact__textSection--right");
+	contactMain.append(contactTextSectionRight);
 }
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,23 @@
 import headerLoader from "./header";
+import hughImg from "./imgs/hugh.png";
 
 export default function menuPageLoad() {
 	headerLoader();
 	const content = document.getElementById("content");
+
+	//  menu-main
+	const menuMain = document.createElement("main");
+	menuMain.classList.add("menu__main");
+	content.append(menuMain);
+
+	//  menu-textContent
+	const menuTextSection = document.createElement("section");
+	menuTextSection.classList.add("menu__textSection");
+	menuMain.append(menuTextSection);
+
+	//  menu-hugh
+	const hugh = new Image();
+	hugh.src = hughImg;
+	hugh.classList.add("menu__hugh");
+	menuMain.append(hugh);
 }
